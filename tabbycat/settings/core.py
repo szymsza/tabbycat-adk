ac5e0901857a591a8a5d97bee3b860b618b2c7c0
Miha Frangež <miha.frangez@gmail.com>
2024-07-07 19:49:47 +0200

    Added environment variables for email and secret key
    This enables Docker deployment without editing soruce files

--- tabbycat/settings/core.py
+++ tabbycat/settings/core.py
@@ -16,7 +16,10 @@ MANAGERS = ADMINS
 DEBUG = bool(int(os.environ['DEBUG'])) if 'DEBUG' in os.environ else False
 ENABLE_DEBUG_TOOLBAR = False # Must default to false; overriden in Dev config
 DISABLE_SENTRY = True # Overriden in Heroku config
+
 SECRET_KEY = r'#2q43u&tp4((4&m3i8v%w-6z6pp7m(v0-6@w@i!j5n)n15epwc'
+if os.environ.get('DJANGO_SECRET_KEY', ''):
+    SECRET_KEY = environ.get('DJANGO_SECRET_KEY')
 
 # ==============================================================================
 # Version
@@ -402,3 +405,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# ==============================================================================
+# Email sending
+# ==============================================================================
+if os.environ.get('EMAIL_HOST', ''):
+    SERVER_EMAIL = os.environ['DEFAULT_FROM_EMAIL']
+    DEFAULT_FROM_EMAIL = os.environ['DEFAULT_FROM_EMAIL']
+    EMAIL_HOST = os.environ['EMAIL_HOST']
+    EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+    EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+    EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+    EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'true').lower() == 'true'
