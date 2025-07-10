4160516b342860becf11943660df34389aaa82b9
Miha Frangež <miha.frangez@gmail.com>
2025-07-10 15:13:12 +0200

    Fix docker behind HTTPS-terminating reverse proxy
    (cherry picked from commit b9bca92e772c782cba99b9f36c3975d79b886359)

--- tabbycat/settings/docker.py
+++ tabbycat/settings/docker.py
@@ -5,6 +5,8 @@
 import os
 
 ALLOWED_HOSTS = ["*"]
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 DATABASES = {
     'default': {
