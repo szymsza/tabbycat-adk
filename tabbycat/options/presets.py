d1e2fcb98f83045837d1dfcb3ca36ee2d8e9dc6a
PiechZ <126514453+PiechZ@users.noreply.github.com>
2026-09-06 18:38:43 +0200

    Change KPDP preset odd-bracket pullup to top instead of random

--- tabbycat/options/presets.py
+++ tabbycat/options/presets.py
@@ -429,7 +429,7 @@ class KPDPPreferences(PreferencesPreset):
     motions__motion_vetoes_enabled             = False
     motions__enable_motion_reuse               = True
     # Draw Rules (penalty-weighted, as with APDA)
-    draw_rules__draw_odd_bracket               = 'pullup_random'
+    draw_rules__draw_odd_bracket               = 'pullup_top'
     draw_rules__draw_pairing_method            = 'adjacent'
     draw_rules__draw_avoid_conflicts           = 'graph'
     draw_rules__draw_pullup_restriction        = 'lowest_ds_wins'
